8579b1ae863196d2e3b30ec1dd4e30df55fe12f1
sb7297 <sbaireddy7297@gmail.com>
2021-07-07 22:05:00 -0400

    Article: step 5

--- components/Article.js
+++ components/Article.js
@@ -86,6 +86,13 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'vanitas',
+    date: '-1',
+    firstParagraph: 'vanitas vanitatum',
+    secondParagraph: 'dixit ecclesiastes',
+    thirdParagraph: 'vanitas vanitatum, et omnia vanitas.'
   }
 ];
 
